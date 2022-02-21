c9f5452d848b0d0e36256cad3d7f52da0003fed9
andrsdt <andresdt@pm.me>
2022-02-21 17:52:48 +0100

    fix: fetch more than 20 posts

--- wallapop.py
+++ wallapop.py
@@ -1,4 +1,3 @@
-from math import prod
 import requests
 import constants as Constants
 from aux import headers_http_get, headers_http_put, generate_body_json
@@ -7,10 +6,23 @@ import colors as Colors
 
 def get_all_product_ids():
     product_ids = []
-    url = f'{Constants.BASE_URL}/web/items/mine/published?init=0'
-    resp = requests.get(url, headers=headers_http_get("items/mine/published",))
-    if resp.status_code == 200:
-        product_ids = [e['id'] for e in resp.json()]
+    offset = 0
+    has_next = True
+
+    # The results are paginated, so we need to iterate over all the pages (size 20)
+    while has_next:
+        url = f'{Constants.BASE_URL}/web/items/mine/published?init={offset}'
+        resp = requests.get(
+            url, headers=headers_http_get("items/mine/published",))
+        if resp.status_code == 200:
+            fetched_product_ids = [e['id'] for e in resp.json()]
+            product_ids += fetched_product_ids
+            has_next = len(fetched_product_ids) == 20
+            offset += 20
+        else:
+            print(Colors.RED + "Error getting product ids from " + url)
+            break
+
     print(Colors.BLUE + f'{len(product_ids)} products found')
     if (len(product_ids) == 0):
         print(Colors.BLUE + "If you have products published, please create the .env file according to the instructions in README.md")
